2c09ed0ad50cf8d6e26cb147b7af91411a26e0d8
Douglas Domingos <doug.domingossilva@gmail.com>
2025-02-25 22:14:12 -0300

    build: add sass package

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/main.scss";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <h1>I'm a teapot!</h1>
-  </StrictMode>,
-)
+  </StrictMode>
+);
