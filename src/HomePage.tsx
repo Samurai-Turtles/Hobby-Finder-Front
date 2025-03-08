97027528efd8004106ca6bc798b4516f84b20b09
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-03-08 19:03:01 -0300

    feat: Adicionando gap entre tags e elementos do container

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,13 +1,24 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Flex } from "@chakra-ui/react";
 import InputSearch from "./components/InputSearch";
 import CustomTag from "./components/CustomTag/CustomTag";
 
 function HomePage() {
   return (
     <Container maxWidth="90vw" py={5}>
-      <InputSearch placeHolder="Buscar evento" />
-      <CustomTag texto="#tag1" visual="solid" />
-      <CustomTag texto="#tag1" visual="outline" />
+      <Flex direction="column" gap={5}>
+        <InputSearch placeHolder="Buscar evento" />
+        <Flex gap={2} wrap="wrap">
+          <CustomTag texto="#tag1" visual="solid" />
+          <CustomTag texto="#tag1" visual="solid" />
+          <CustomTag texto="#tag1" visual="solid" />
+          <CustomTag texto="#tag1" visual="solid" />
+          <CustomTag texto="#tag1" visual="solid" />
+          <CustomTag texto="#tag1" visual="solid" />
+          <CustomTag texto="#tag1" visual="solid" />
+          <CustomTag texto="#tag1" visual="solid" />
+          <CustomTag texto="#tag1" visual="outline" />
+        </Flex>
+      </Flex>
     </Container>
   );
 }
