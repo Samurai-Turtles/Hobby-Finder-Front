52a8cfaacc6f865eee6505e4f916b1970ebd3660
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-03-08 19:22:23 -0300

    feat: Criação do card de evento (sem componente da privacidade)

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Container, Flex } from "@chakra-ui/react";
 import InputSearch from "./components/InputSearch";
 import CustomTag from "./components/CustomTag/CustomTag";
+import EventCard from "./components/EventCard";
 
 function HomePage() {
   return (
@@ -18,6 +19,15 @@ function HomePage() {
           <CustomTag texto="#tag1" visual="solid" />
           <CustomTag texto="#tag1" visual="outline" />
         </Flex>
+        <EventCard
+          imgSrc="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          nomeEvento="Nome"
+          descricao="descricao"
+          localizacao="local"
+          distancia={2}
+          dataInicial="data inicial"
+          dataFinal="data final"
+        />
       </Flex>
     </Container>
   );
---
+++ src/components/EventCard.tsx
@@ -0,0 +1,45 @@
+import { Button, Card, Flex, Image, Text } from "@chakra-ui/react";
+import { CalendarDots, MapPin } from "@phosphor-icons/react";
+
+interface EventCardProps {
+  imgSrc: string;
+  nomeEvento: string;
+  descricao: string;
+  localizacao: string;
+  distancia: number;
+  dataInicial: string;
+  dataFinal: string;
+}
+
+function EventCard({
+  imgSrc,
+  nomeEvento,
+  descricao,
+  localizacao,
+  distancia,
+  dataInicial,
+  dataFinal,
+}: EventCardProps) {
+  return (
+    <Card.Root minW="sm" overflow="hidden">
+      <Image src={imgSrc} alt="Imagem do evento" maxH="10rem" />
+      <Card.Body gap="1" p={2}>
+        <Card.Title>{nomeEvento}</Card.Title>
+        <Card.Description>{descricao}</Card.Description>
+        <Flex align="center" gap={1}>
+          <MapPin size={16} />
+          <Text fontSize="sm" fontWeight="medium" letterSpacing="tight">
+            {localizacao} ({distancia} km)
+          </Text>
+        </Flex>
+        <Flex align="center" gap={1}>
+          <CalendarDots size={16} />
+          <Text fontSize="sm" fontWeight="medium" letterSpacing="tight">
+            {dataInicial} até {dataFinal}
+          </Text>
+        </Flex>
+      </Card.Body>
+    </Card.Root>
+  );
+}
+export default EventCard;
