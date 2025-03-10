c497cfc1bd5f4d50ee37d6356423cd292c7fb503
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-03-10 07:41:28 -0300

    fix: Ajustando tamanho de imagem de perfil e posição de componentes de tags

--- src/EditarPerfil.tsx
+++ src/EditarPerfil.tsx
@@ -1,7 +1,6 @@
 import {
   Avatar,
   Box,
-  Button,
   Container,
   Field,
   Fieldset,
@@ -22,13 +21,14 @@ function EditarPerfil() {
 
   return (
     <Container maxWidth="90vw" py={5}>
+      COLOCAR COMPONENTE DE VOLTAR TELA AQUI
       <Flex direction="column" minW="100%" alignItems="center" gap={5}>
         <Box position="relative">
-          <Avatar.Root size="2xl">
+          <Avatar.Root minW="8rem" minH="8rem">
             <Avatar.Fallback name="Username" />
             <Avatar.Image src="#" />
           </Avatar.Root>
-          <Float placement="bottom-end" offsetY={2}>
+          <Float placement="bottom-end" offset={4}>
             <button style={{ cursor: "pointer" }}>
               <Box bg="customOrange" color="customWhite" p={1} rounded="md">
                 <Camera size={25} />
@@ -36,9 +36,14 @@ function EditarPerfil() {
             </button>
           </Float>
         </Box>
-        <button style={{ cursor: "pointer" }}>
-          <Plus size={32} />
-        </button>
+        <Flex alignItems="center" gap={2}>
+          COLOCAR OS COMPONENTES DAS TAGS AQUI
+          <button style={{ cursor: "pointer" }}>
+            <Box border="1px solid" color="customOrange" p={1} rounded="full">
+              <Plus size={15} />
+            </Box>
+          </button>
+        </Flex>
         <Fieldset.Root size="lg" maxW="md">
           <Fieldset.Content>
             {inputs.map((e) => {
