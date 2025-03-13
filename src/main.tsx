83ec07430df249336c8e6bbd2cedf35d26f2ee39
Hildon27 <hildon.regis.navarro.neto@ccc.ufcg.edu.br>
2025-03-12 21:04:21 -0300

    feat: notification screen

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider";
 import { Box } from "@chakra-ui/react";
+import NotificationScreen from "./page/NotificationScreen";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <Box bg="orange">
-        <h1>I'm a teapot!</h1>
-      </Box>
+      <NotificationScreen />
     </Provider>
   </StrictMode>,
 );
---
+++ src/page/NotificationScreen.tsx
@@ -0,0 +1,50 @@
+import Notification from "@/components/Notification";
+import { Button, Container, VStack } from "@chakra-ui/react";
+import { CaretLeft } from "@phosphor-icons/react";
+
+function NotificationScreen() {
+  return (
+    <Container maxWidth="90vw" paddingY="5">
+      <Button
+        bg="customOrange"
+        color="customWhite"
+        rounded="md"
+        gap="0"
+        paddingLeft="2"
+      >
+        <CaretLeft size={32} />
+        Voltar
+      </Button>
+      <VStack alignItems="left" marginY="5">
+        <Notification
+          imgSrc="https://github.com/Hildon27.png"
+          msg="Hildon deseja participar do evento Domino em Seu Helio"
+          isSolicitation={true}
+        />
+
+        <Notification
+          imgSrc="https://github.com/Hildon27.png"
+          msg="Tomara que dê certo"
+        />
+
+        <Notification
+          imgSrc="https://github.com/johndoe.png"
+          msg="Maria comentou no evento Futebol de Sábado"
+        />
+
+        <Notification
+          imgSrc="https://github.com/janedoe.png"
+          msg="Carlos confirmou presença no evento Churrasco do Fim de Semana"
+        />
+
+        <Notification
+          imgSrc="https://github.com/randomuser.png"
+          msg="Ana enviou um convite para o evento Corrida no Parque"
+          isSolicitation={true}
+        />
+      </VStack>
+    </Container>
+  );
+}
+
+export default NotificationScreen;
