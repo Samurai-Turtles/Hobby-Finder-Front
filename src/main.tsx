da5f328099c71cfb9a5ba60de8614a5184bda7f3
Hildon27 <hildon.regis.navarro.neto@ccc.ufcg.edu.br>
2025-03-17 23:10:21 -0300

    feat: cancel event view screen

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,14 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider";
 import { Box, Container } from "@chakra-ui/react";
 import Header from "./components/Header";
+import CancelEventViewScreen from "./pages/CancelEventViewScreen";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
       <Header />
       <Container maxWidth="90vw">
-        <Box>
-          <h1>Hello Word!</h1>
-        </Box>
+        <CancelEventViewScreen />
       </Container>
     </Provider>
   </StrictMode>,
