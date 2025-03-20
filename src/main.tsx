632f123d3fb366ae6252032c23c0701dce99fb2a
Rossini Caroca <rossini.vilela.caroca@ccc.ufcg.edu.br>
2025-03-20 12:32:02 -0300

    feat: Private Event View Solicitation Invited Screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider";
-import { Box, Container } from "@chakra-ui/react";
+//import { Box, Container } from "@chakra-ui/react";
 import Header from "./components/layout/Header/Header";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import Recovery from "./pages/Recovery";
-import HomePage from "./pages/HomePage";
-import UserProfile from "./pages/UserProfile";
-import EditUserProfile from "./pages/EditUserProfile";
-import Notifications from "./pages/Notifications";
-import Participants from "./pages/Participants";
-import Solicitations from "./pages/Solicitations";
-import EventRatings from "./pages/EventRatings";
-
+//import Login from "./pages/Login";
+//import SignUp from "./pages/SignUp";
+//import Recovery from "./pages/Recovery";
+//import HomePage from "./pages/HomePage";
+//import UserProfile from "./pages/UserProfile";
+//import EditUserProfile from "./pages/EditUserProfile";
+//import Notifications from "./pages/Notifications";
+//import Participants from "./pages/Participants";
+//import Solicitations from "./pages/Solicitations";
+//import EventRatings from "./pages/EventRatings";
+import PrivateEventViewSolicitationInvited from "./pages/PrivateEventViewSolicitationInvited";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <Provider defaultTheme="light">
       <Header />
-      <EventRatings />
+      <PrivateEventViewSolicitationInvited />
     </Provider>
   </StrictMode>,
 );
