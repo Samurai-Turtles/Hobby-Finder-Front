164406c0a0ab19077a74fc9ab1339fee2f78d501
Douglas Domingos <doug.domingossilva@gmail.com>
2025-04-06 19:59:20 -0300

    feat: add remote backend address

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://hobby-finder-back-1-0.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
