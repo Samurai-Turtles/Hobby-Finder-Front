0160ca2fcffdbc18d137d6b85363978c3a3f00db
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-04-06 18:53:50 -0300

    feat: Integrando funcionalidades de visualizar participantes, promover/despromover organizador e remover participantes

--- src/components/cards/ParticipantProfileCard.tsx
+++ src/components/cards/ParticipantProfileCard.tsx
@@ -2,18 +2,66 @@ import { Participant } from "@/pages/Participants";
 import { Avatar, Box, Card, CloseButton, Flex, Text } from "@chakra-ui/react";
 import { Star } from "@phosphor-icons/react";
 import ActionButton from "../buttons/action-button";
+import api from "@/api/axiosConfig";
 
 interface ParticipantProfileCardProps {
+  idEvent: string;
   display: string;
   participant: Participant;
   closeClick: (e: any) => void;
 }
 
 function ParticipantProfileCard({
+  idEvent,
   display,
   participant,
   closeClick,
 }: ParticipantProfileCardProps) {
+  const handleConvertToOrganizer = async () => {
+    try {
+      await api.put(
+        `/event/${idEvent}/participation/${participant.participationId}/update-presence`,
+        {
+          participation: participant.participationStatus,
+          position: "ADMIN",
+        },
+      );
+      window.location.reload();
+    } catch (error) {
+      console.log(
+        `Erro ao tornar participante organizador (ID: ${participant.id})`,
+      );
+    }
+  };
+
+  const handleConvertToParticipant = async () => {
+    try {
+      await api.put(
+        `/event/${idEvent}/participation/${participant.participationId}/update-presence`,
+        {
+          participation: participant.participationStatus,
+          position: "PARTICIPANT",
+        },
+      );
+      window.location.reload();
+    } catch (error) {
+      console.log(
+        `Erro ao tornar organizador participante (ID: ${participant.id})`,
+      );
+    }
+  };
+
+  const handleRemoveParticipant = async () => {
+    try {
+      await api.delete(
+        `/event/${idEvent}/participation/${participant.participationId}`,
+      );
+      window.location.reload();
+    } catch (error) {
+      console.log(`Erro ao remover participante (ID: ${participant.id})`);
+    }
+  };
+
   return (
     <Box
       display={display}
@@ -28,31 +76,41 @@ function ParticipantProfileCard({
           <Card.Body paddingBottom="1.2rem" gap="2">
             <Avatar.Root alignSelf="center" size="2xl">
               <Avatar.Image src={participant.srcImgProfile} />
-              <Avatar.Fallback name={participant.nickname} />
+              <Avatar.Fallback name={participant.participantData.username} />
             </Avatar.Root>
-            <Card.Title textAlign="center">{participant.nickname}</Card.Title>
+            <Card.Title textAlign="center">
+              {participant.participantData.username}
+            </Card.Title>
             <Flex justifyContent="center" gap={1}>
-              <Text>{participant.avaliacao}/5</Text>
+              <Text>{participant.participantData.avaliacao}/5</Text>
               <Text color="customOrange">
                 <Star size={20} weight="fill" />
               </Text>
             </Flex>
-            <Card.Description>{participant.bio}</Card.Description>
+            <Card.Description>
+              {participant.participantData.bio}
+            </Card.Description>
           </Card.Body>
           <Card.Footer flexDirection="column">
-            {participant.tipo.includes("PARTICIPANTE") ? (
-              <ActionButton label="Tornar Organizador" action={() => {}} />
+            {participant.situation.includes("PARTICIPANTE_CONFIRMADO") ? (
+              <ActionButton
+                label="Tornar Organizador"
+                action={handleConvertToOrganizer}
+                w="full"
+              />
             ) : (
               <ActionButton
                 label="Remover como Organizador"
                 buttonStyle="alert"
-                action={() => {}}
+                action={handleConvertToParticipant}
+                w="full"
               />
             )}
             <ActionButton
               label="Remover Participante"
               buttonStyle="alert"
-              action={() => {}}
+              action={handleRemoveParticipant}
+              w="full"
             />
           </Card.Footer>
         </Card.Root>
